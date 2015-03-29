fad3c7aa2488773878fd4a5a007bc8ff6afdd733
Dennis Yatunin <dyatun@gmail.com>
2015-03-29 01:50:38 -0400

    Update: Efficiency has been improved :smiling_imp:

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -1,11 +1,14 @@
 import java.util.NoSuchElementException;
+
 public class MyDeque<E> {
 
 	private E[] objects;
-	private int head, size, capacity;
+	private int head, tail, size, capacity;
 
 	@SuppressWarnings("unchecked")
 	public MyDeque(int c) {
+		if (c < 1)
+			throw new IllegalArgumentException();
 		objects = (E[]) new Object[c];
 		capacity = c;
 	}
@@ -19,20 +22,32 @@ public class MyDeque<E> {
 	@SuppressWarnings("unchecked")
 	private void resize() {
 		E[] doubleObjects = (E[]) new Object[2 * capacity];
-		for (int i = 0; i < size; i++)
-			doubleObjects[i] = objects[(head + i) % capacity];
+		if (size < 25)
+			for (int i = 0; i < size; i++)
+				doubleObjects[i] = objects[(head + i) % capacity];
+		else {
+			if (tail >= head)
+				System.arraycopy(objects, head, doubleObjects, 0, size);
+			else {
+				System.arraycopy(objects, head, doubleObjects, 0, capacity - head);
+				System.arraycopy(objects, 0, doubleObjects, capacity - head, size - capacity + head);
+			}
+		}
 		objects = doubleObjects;
 		head = 0;
+		tail = size - 1;
 		capacity *= 2;
 	}
 
 	public void addFirst(E e) {
 		if (size == capacity)
 			resize();
-		if (head == 0)
-			head = capacity - 1;
-		else
-			head = (head - 1) % capacity;
+		if (size > 0) {
+			if (head == 0)
+				head = capacity - 1;
+			else
+				head--;
+		}
 		objects[head] = e;
 		size++;
 	}
@@ -40,15 +55,29 @@ public class MyDeque<E> {
 	public void addLast(E e) {
 		if (size == capacity)
 			resize();
-		objects[(head + size) % capacity] = e;
+		if (size > 0) {
+			if (tail == capacity - 1)
+				tail = 0;
+			else
+				tail++;
+		}
+		objects[tail] = e;
 		size++;
+	}
+
+	public boolean add(E item) {
+		addLast(item);
+		return true;
 	}
 
 	public E removeFirst() {
 		if (size == 0)
 			throw new NoSuchElementException();
 		E value = objects[head];
-		head = (head + 1) % capacity;
+		if (head == capacity - 1)
+			head = 0;
+		else
+			head++;
 		size--;
 		return value;
 	}
@@ -56,7 +85,11 @@ public class MyDeque<E> {
 	public E removeLast() {
 		if (size == 0)
 			throw new NoSuchElementException();
-		E value = objects[(head + size - 1) % capacity];
+		E value = objects[tail];
+		if (tail == 0)
+			tail = capacity - 1;
+		else
+			tail--;
 		size--;
 		return value;
 	}
@@ -70,7 +103,7 @@ public class MyDeque<E> {
 	public E getLast() {
 		if (size == 0)
 			throw new NoSuchElementException();
-		return objects[(head + size - 1) % capacity];
+		return objects[tail];
 	}
 
 	public int size() {
@@ -81,7 +114,7 @@ public class MyDeque<E> {
 		String result = "[ ";
 		for (int i = 0; i < size - 1; i++)
 			result += objects[(head + i) % capacity] + ", ";
-		return result + objects[(head + size - 1) % capacity] + " ]";
+		return result + objects[tail] + " ]";
 	}
 
 }
